64ebcb68e60bfb93198e09499229baf443213a5a
Sharad <sharad.bhalla@rsystems.com>
2012-04-20 10:26:03 +0530

    so much fun

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,7 +2,7 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is fun");
+		System.out.println("Git is so much fun");
 	}
 }
 
