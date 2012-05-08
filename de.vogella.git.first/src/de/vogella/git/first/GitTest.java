f3c363f194c3ecb2bf8b56134c2e741f2f695cdd
Sharad <sharad.bhalla@rsystems.com>
2012-05-08 15:11:24 +0530

    Hi

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -2,7 +2,7 @@ package de.vogella.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git is so much fun")
+		System.out.println("Git is wonderful")
 	}
 }
 
